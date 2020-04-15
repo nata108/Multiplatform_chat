2fdcda70b26cbb2df7a89f829b1573b4e3b95741
Gyöngyössy Natabara <natabara@gyongyossy.hu>
2020-04-15 17:28:14 +0200

    Build verzió

---
+++ build/precache-manifest.7e456104fbb7555665491ba06c2a15db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27121b2b7593b147201ca0c67fcb5299",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bdb178640afb5fda170d",
+    "url": "./static/css/main.2a2fbe87.chunk.css"
+  },
+  {
+    "revision": "f9f1c85ed2dfb059483d",
+    "url": "./static/js/2.db39bea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db39bea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb178640afb5fda170d",
+    "url": "./static/js/main.ee3faadb.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
